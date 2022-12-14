774b93597d298b97ba38bb957694543c330bcd0f
LHWN <cjsrnr1674@hanmail.net>
2022-12-14 09:03:18 +0900

    complete ButtonScript Func and WcsScript Func

--- popup.js
+++ popup.js
@@ -1,23 +1,40 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         var result = request.result;
+        var message = request.message;
 
-        console.log(request.result);
-        console.log(request.message);
+        console.log(result.message);
 
-        if(result.length > 0) {
-            result.forEach(element => {
-                // 추가할 li element 생성
-                const li = document.createElement('li');
-    
-                // li 에 textnode 추가
-                const textnode = document.createTextNode(element);
-                li.appendChild(textnode);
-    
-                document.getElementById('buttonScript').appendChild(li);
-            });
-        } else {
-            document.getElementById('buttonScriptComponent').innerText = "Not imported"
+        if(message === 'SUCCESS_BUTTON') {
+            if(result.length > 0) {
+                // var result = request.result;
+                result.forEach(element => {
+                    // 추가할 li element 생성
+                    const li = document.createElement('li');
+        
+                    // li 에 textnode 추가
+                    const textnode = document.createTextNode(element);
+                    li.appendChild(textnode);
+        
+                    document.getElementById('buttonScript').appendChild(li);
+                });
+            } else {
+                document.getElementById('buttonScriptComponent').innerText = "Not imported";
+            }
+        } else if(message === 'SUCCESS_WCS') {
+            if(result.length > 0) {
+                // var result = request.result;
+                result.forEach(element => {
+                    const li = document.createElement('li');
+
+                    const textnode = document.createTextNode(element);
+                    li.appendChild(textnode);
+
+                    document.getElementById('wcsScript').appendChild(li);
+                })
+            } else {
+                document.getElementById('wcsScriptComponent').innerText = "Not imported";
+            }
         }
         sendResponse({ 
             message: "OK"
