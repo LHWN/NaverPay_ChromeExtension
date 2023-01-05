e0f8ab20aaf3af6e6ade4b0124e140f019468164
LHWN <cjsrnr1674@hanmail.net>
2023-01-05 09:29:38 +0900

    probably completed a ... func of shopping

--- searchShopping.js
+++ searchShopping.js
@@ -10,24 +10,25 @@ var search = function(merchantName, merchantUrl, url) {
 
             for (let index = 0; index < products.length; index++) {
                 const element = products[index].item;
-                
+
                 console.log(element);
-                if(element.item.hasOwnProperty('mallPcUrl')) {
+                console.log(element.hasOwnProperty('mallPcUrl'));
+                if(element.hasOwnProperty('mallPcUrl')) {
                     var mallPcUrl = element.mallPcUrl;
                     if(mallPcUrl.search(merchantUrl) != -1) {
-                        result = element.item.id;
-                        console.log(element);
+                        result = element.id;
+                        console.log(result);
+                        document.getElementById('resultSection').style.display = 'block';
+                        document.getElementById('resultSection').innerText = result;
+                        document.getElementById('resultSection').innerHTML = '<button class="btn btn-outline-secondary" type="button" id="buttonGoShoppingResult">보러가기</button>';
+
                         break;
                     }
                 }
             }
-
-            alert(result);
         }
     };
     xhr.send();
-//093316ez.toolpark.kr
-    return result;
 }
 
 
